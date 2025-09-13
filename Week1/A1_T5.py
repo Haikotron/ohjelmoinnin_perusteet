ba9751c65ea8eb1510c8a1685fd8347d087b758e
Haikotron <mikael.haiko@gmail.com>
2025-09-13 19:48:47 +0300

    Tehtava 5

---
+++ Week1/A1_T5.py
@@ -0,0 +1,9 @@
+print("Calculate the are of a wall.")
+Feed = input("Enter the width in meters: ")
+Width = int(Feed)
+Feed = input("Enter the height in meters: ")
+Height = int(Feed)
+print("Width is", Width, "m and height is", Height, "m")
+Area = Width * Height
+print("The wall will be", Area, "square meters.")
+
