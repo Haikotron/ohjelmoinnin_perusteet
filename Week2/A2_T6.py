49e3bbfc774e78699c43c16b541abfd00d61a6a1
Antti Haiko <mikael.haiko@gmail.com>
2025-09-22 16:49:53 +0300

    Update A2_T6.py

--- Week2/A2_T6.py
+++ Week2/A2_T6.py
@@ -1,12 +1,4 @@
-#Write a Python program which asks user to insert hex color.
-#In this case hex color is expected to be the 7 character representation
-# starting with # and followed by 6 0-F characters to represent RGB colors.
-# More about hex colors at https://en.wikipedia.org/wiki/Web_colors
-
-#Slice the amount of red, green and blue
-#from that inserted color and display each color as shown below.
-
-print("Program starting. \n")
+print("Program starting.\n")
 hex = input("Insert a hex color: ")
 #kysytään käyttäjältä hex-koodi väri
 print("\n Colors")
@@ -17,5 +9,3 @@ print(f"- Green {hex[3:5]}")
 print(f"- Blue {hex[5:7]}\n")
 #ensimmäiset 5 merkkiä pois, 6 ja 7 merkki printtaantuu
 print("Program ending.")
-
-
